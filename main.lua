f0ecf2904aa417e7e4309d224ef7f8b049d6d5f4
Ab Aeterno <alitomosca@gmail.com>
2024-08-11 00:27:20 -0300

    Disable debug options

--- main.lua
+++ main.lua
@@ -7,9 +7,9 @@ include("PST_config")
 PST:resetData()
 
 PST.debugOptions = {
-	infSP = true, -- No longer spend or require skill points for nodes
-	infRespec = true, -- No longer spend or require respec points for nodes
-	allAvailable = true, -- Makes all nodes available
+	infSP = false, -- No longer spend or require skill points for nodes
+	infRespec = false, -- No longer spend or require respec points for nodes
+	allAvailable = false, -- Makes all nodes available
 	drawNodeIDs = false, -- Draw node IDs on the tree screen
 }
 
