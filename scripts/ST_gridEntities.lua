66bde7e2e7a77f7c5920b4a2b722682c5c9ff9ff
Ab Aeterno <alitomosca@gmail.com>
2024-08-11 15:22:10 -0300

    Improve how static entities are stored

--- scripts/ST_gridEntities.lua
+++ scripts/ST_gridEntities.lua
@@ -1,10 +1,13 @@
 function PST:getStaticEntityID(entity)
     local stage = Game():GetLevel():GetStage()
-    local roomID = Game():GetLevel():GetCurrentRoomIndex()
+    local roomID = Game():GetLevel():GetCurrentRoomDesc().SafeGridIndex
     if entity.GetGridIndex then
         return tostring(stage) .. "." .. tostring(roomID) .. "." .. tostring(entity:GetGridIndex())
+    elseif entity.SpawnGridIndex then
+        return tostring(stage) .. "." .. tostring(roomID) .. "." .. tostring(entity.SpawnGridIndex)
     end
-    return tostring(stage) .. "." .. tostring(roomID) .. "." .. tostring(math.floor(entity.Position.X / 40)) .. "." .. tostring(math.floor(entity.Position.Y / 40))
+    local tmpGridIndex = Game():GetRoom():GetGridIndex(entity.Position)
+    return tostring(stage) .. "." .. tostring(roomID) .. "." .. tostring(tmpGridIndex)
 end
 
 function PST:initStaticEntity(entity)
@@ -12,20 +15,21 @@ function PST:initStaticEntity(entity)
     if staticEntCache then
         local entityID = PST:getStaticEntityID(entity)
         if not staticEntCache[entityID] then
-            staticEntCache[entityID] = { State = -1 }
+            staticEntCache[entityID] = -1
         end
-        return staticEntCache[entityID]
+        return entityID
     end
     return nil
 end
 
 function PST:gridEntityPoopUpdate(entityParam)
-    local entity = PST:initStaticEntity(entityParam)
-    if entity then
-        if entity.State < entityParam.State then
-            entity.State = entityParam.State
+    local entityID = PST:initStaticEntity(entityParam)
+    if entityID then
+        local staticEntCache = PST:getTreeSnapshotMod("staticEntitiesCache", nil)
+        if staticEntCache[entityID] < entityParam.State then
+            staticEntCache[entityID] = entityParam.State
             -- Poop destroyed
-            if entity.State >= 1000 then
+            if staticEntCache[entityID] >= 1000 then
                 -- Mod: +xp when destroying poop
                 local tmpMod = PST:getTreeSnapshotMod("poopXP", 0)
                 if tmpMod > 0 then
@@ -39,12 +43,13 @@ end
 function PST:gridEntityRockUpdate(entityParam)
     -- Tinted rocks
     if entityParam.Desc.Type == GridEntityType.GRID_ROCKT then
-        local entity = PST:initStaticEntity(entityParam)
-        if entity then
-            if entity.State ~= entityParam.State then
-                entity.State = entityParam.State
+        local entityID = PST:initStaticEntity(entityParam)
+        if entityID then
+            local staticEntCache = PST:getTreeSnapshotMod("staticEntitiesCache", nil)
+            if staticEntCache[entityID] ~= entityParam.State then
+                staticEntCache[entityID] = entityParam.State
                 -- Tinted rock destroyed
-                if entity.State >= 2 then
+                if staticEntCache[entityID] >= 2 then
                     -- Mod: +xp when destroying tinted rocks
                     local tmpMod = PST:getTreeSnapshotMod("tintedRockXP", 0)
                     if tmpMod ~= 0 then
@@ -63,18 +68,19 @@ function PST:gridEntityRockUpdate(entityParam)
 end
 
 function PST:onFireRender(entityParam)
-    local entity = PST:initStaticEntity(entityParam)
-    if entity then
-        if entity.State ~= entityParam.HitPoints then
+    local entityID = PST:initStaticEntity(entityParam)
+    if entityID then
+        local staticEntCache = PST:getTreeSnapshotMod("staticEntitiesCache", nil)
+        if staticEntCache[entityID] ~= entityParam.HitPoints then
             -- Fireplace destroyed
-            if entity.State > 1 and entityParam.HitPoints <= 1 then
+            if staticEntCache[entityID] > 1 and entityParam.HitPoints <= 1 then
                 -- Mod: +xp when destroying a fireplace
                 local tmpBonus = PST:getTreeSnapshotMod("fireXP", 0)
                 if tmpBonus ~= 0 then
                     PST:addTempXP(tmpBonus, true)
                 end
             end
-            entity.State = entityParam.HitPoints
+            staticEntCache[entityID] = entityParam.HitPoints
         end
     end
 end
