048d5472b8659669bde3add380506204e27525d6
Ab Aeterno <alitomosca@gmail.com>
2024-08-17 17:55:01 -0300

    Adjust "less chance to find devil room" modifier calc

--- scripts/ST_devilChance.lua
+++ scripts/ST_devilChance.lua
@@ -2,7 +2,7 @@ function PST:applyDevilChance(chance)
     local treeDevilChance = PST:getTreeSnapshotMod("devilChance", 0) / 100
 
     -- Starcursed mod: -% chance to find a devil/angel room
-    local starcursedChance = PST:SC_getSnapshotMod("lessDevilRoomChance", 0)
+    local starcursedChance = PST:SC_getSnapshotMod("lessDevilRoomChance", 0) / 100
 
     return math.max(0, chance + treeDevilChance - starcursedChance)
 end
