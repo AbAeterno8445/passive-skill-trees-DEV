18b55d57c521c3822b446fec3e12ac909dc1c2bd
Ab Aeterno <alitomosca@gmail.com>
2024-08-26 14:26:10 -0300

    Reset release directories before building

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
+if exist "./releases/passive skill trees/content" rmdir /s /q "./releases/passive skill trees/content"
+if exist "./releases/passive skill trees/resources" rmdir /s /q "./releases/passive skill trees/resources"
+if exist "./releases/passive skill trees/scripts" rmdir /s /q "./releases/passive skill trees/scripts"
+
 if not exist "./releases/passive skill trees" mkdir "./releases/passive skill trees"
 if not exist "./releases/passive skill trees/resources" mkdir "./releases/passive skill trees/resources"
 xcopy /s /y /i /exclude:build_exclude.txt "./resources" "./releases/passive skill trees/resources/"
