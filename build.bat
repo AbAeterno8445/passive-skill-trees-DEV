277068626a733f5f93c39fe948d98addc838de15
Ab Aeterno <alitomosca@gmail.com>
2024-09-30 16:39:18 -0300

    Remove metadata from dev dir, keeping release one

--- build.bat
+++ build.bat
@@ -13,6 +13,5 @@ if not exist "./releases/passive skill trees/content" mkdir "./releases/passive 
 xcopy /s /y /i "./content" "./releases/passive skill trees/content/"
 
 xcopy /y /i "main.lua" "./releases/passive skill trees/main.lua"
-xcopy /y /i "metadata.xml" "./releases/passive skill trees/metadata.xml"
 xcopy /y /i "PST_config.lua" "./releases/passive skill trees/PST_config.lua"
 xcopy /y /i "thumb.png" "./releases/passive skill trees/thumb.png"
