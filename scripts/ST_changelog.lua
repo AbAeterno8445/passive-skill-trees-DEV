8c8d2e8c43e3cce16d38f2fa33f54a69f86ee9f8
Ab Aeterno <alitomosca@gmail.com>
2024-11-10 23:32:48 -0300

    Fix Lost Coral Trident pickup

--- scripts/ST_changelog.lua
+++ scripts/ST_changelog.lua
@@ -7,6 +7,7 @@ function PST:getChangelogList()
         "- Added a new Ancient Jewel: \"Glittering Starstone\".",
         "- Adjusted Sidereal Caches' internal ID to potentially avoid conflicts.",
         "- Expeditions' Curse of Punishment damage should no longer cause penalties such as reducing devil deal chance.",
+        "- Fixed Lost Coral Trident (ancient weapon) not being registered when picked up.",
         "- Fixed final expedition nodes occasionally being replaced by different node types.",
         "- Fixed cases where completing the last node in an expedition wouldn't reset it and unlock the next depth.",
         "- Fixed chests that give items when opened not counting towards expedition objectives.",
