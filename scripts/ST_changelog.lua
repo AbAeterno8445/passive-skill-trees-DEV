40a046e8879bd1e2758b575950fd8768155f803e
Ab Aeterno <alitomosca@gmail.com>
2025-03-30 15:37:45 -0300

    Changelog update

--- scripts/ST_changelog.lua
+++ scripts/ST_changelog.lua
@@ -4,7 +4,7 @@ function PST:getChangelogList()
         "",
 
         "v1.2.3",
-        "- Clearing the same room multiple times now applies a stacking xp penalty to that room.",
+        "- Clearing the same room multiple times now applies a stacking xp penalty to that room (such as Community Remix's hexed rooms).",
         "- Refactored code logic for room clears, should generally cause less jank in cases where monsters spawn on cleared rooms.",
         "- Reduced Shadowmeld's cooldown to 7 seconds (from 12) (Siren tree).",
         "- Shadowmeld now has quality 1 and EID description, in case it ever needs to show up as an item.",
