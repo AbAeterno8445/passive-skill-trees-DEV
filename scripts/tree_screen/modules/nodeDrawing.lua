0e612f947e773c332a0b28274f599e7cd44e1d1f
Ab Aeterno <alitomosca@gmail.com>
2025-04-01 15:24:02 -0300

    Fix node visibility extra func check

--- scripts/tree_screen/modules/nodeDrawing.lua
+++ scripts/tree_screen/modules/nodeDrawing.lua
@@ -55,7 +55,7 @@ local function PST_isNodeVisible(node, charData)
     end
     -- Extra node visibility funcs
     for _, tmpFunc in pairs(nodeVisibleExtraFuncs) do
-        if tmpFunc and not tmpFunc(node) then return false end
+        if tmpFunc and tmpFunc(node) == false then return false end
     end
     return true
 end
